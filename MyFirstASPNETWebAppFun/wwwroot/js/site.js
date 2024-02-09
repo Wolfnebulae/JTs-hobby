be18b6257866c54cd1f42fb73278770e73aa4086
jwtay <legocrazy00@gmail.ccom>
2024-02-08 20:20:38 -0700

    Add project files.

---
+++ MyFirstASPNETWebAppFun/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#calculate").click(function () {
+        let inputHours = parseInt($("#hours").val());
+        if (isNaN(inputHours) || inputHours < 0) {
+            alert("Please input a number into the hours box");
+        } else {
+            let total = inputHours * 10.98;
+            $("#total").text("$" + total.toFixed(2));
+        }
+    });
+});
